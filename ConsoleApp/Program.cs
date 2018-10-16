923019c73e33824f9942da46942f62b8d27853f2
StevePro7 <steveprozune@hotmail.com>
2018-10-16 08:37:36 +0100

    Initial checkin

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using WindowsGame.Common.Data;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LevelConfigData data = new LevelConfigData
+            {
+                LevelNo = 1,
+                LevelName = "MOOT",
+                BonusLevel = false,
+                GridDelay = 200,
+                BulletMaxim = 5,
+                BulletFrame = 10,
+                BulletShoot = 50,
+                EnemySpawn = 1,
+                EnemyTotal = 5,
+            };
+
+            using (Stream stream = new FileStream("steve.xml", FileMode.Create))
+            {
+                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(LevelConfigData));
+                serializer.Serialize(stream, data);
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
