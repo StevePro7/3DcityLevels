fef7bbf62a3b0f0b1bc22b510fdc756eec63493f
StevePro7 <steveprozune@hotmail.com>
2018-10-16 11:53:45 +0100

    Add UTF-8 and ignore namespace

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml.Serialization;
 using WindowsGame.Common.Data;
 
 namespace ConsoleApp
@@ -19,17 +20,39 @@ namespace ConsoleApp
                 BulletShoot = 50,
                 EnemySpawn = 1,
                 EnemyTotal = 5,
+                EnemyFrameDelay = 100,
             };
 
-            using (Stream stream = new FileStream("steve.xml", FileMode.Create))
-            {
+            const string fileName = "steve.xml";
 
-                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(LevelConfigData));
-                //https://stackoverflow.com/questions/625927/omitting-all-xsi-and-xsd-namespaces-when-serializing-an-object-in-net
-                System.Xml.Serialization.XmlSerializerNamespaces ns = new System.Xml.Serialization.XmlSerializerNamespaces();
-                ns.Add(String.Empty, String.Empty);
-                serializer.Serialize(stream, data, ns);
+            // UTF8
+            https://stackoverflow.com/questions/3862063/serializing-an-object-as-utf-8-xml-in-net
+
+            // Namespace
+            //https://stackoverflow.com/questions/625927/omitting-all-xsi-and-xsd-namespaces-when-serializing-an-object-in-net
+
+            using (Stream stream = new FileStream(fileName, FileMode.Create))
+            {
+                using (var writer = new StreamWriter(stream, System.Text.Encoding.UTF8))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(LevelConfigData));
+                    XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                    ns.Add(String.Empty, String.Empty);
+
+                    serializer.Serialize(writer, data, ns);
+                }
+
             }
+
+            //using (Stream stream = new FileStream("steve.xml", FileMode.Create))
+            //{
+
+            //    System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(LevelConfigData));
+            //    //https://stackoverflow.com/questions/625927/omitting-all-xsi-and-xsd-namespaces-when-serializing-an-object-in-net
+            //    System.Xml.Serialization.XmlSerializerNamespaces ns = new System.Xml.Serialization.XmlSerializerNamespaces();
+            //    ns.Add(String.Empty, String.Empty);
+            //    serializer.Serialize(stream, data, ns);
+            //}
 
 
             Console.WriteLine("Hello World!");
