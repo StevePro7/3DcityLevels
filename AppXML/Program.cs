b91574ccc53d0ad6b004b1989d6e08d86cb13f3c
StevePro7 <steveprozune@hotmail.com>
2018-10-19 07:56:32 +0100

    Additional apps

---
+++ AppXML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
