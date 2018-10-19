a8dd9381588cec143f61483ee2e5922ed56294f1
StevePro7 <steveprozune@hotmail.com>
2018-10-19 07:56:42 +0100

    Additional files

---
+++ ClassLibrary/FileIO/FileMgr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.FileIO
+{
+    public class FileMgr
+    {
+        public const string InpDir = "Inp";
+        public const string OutDir = "Out";
+        public const string EasySub = "Easy";
+        public const string HardSub = "Hard";
+    }
+}
